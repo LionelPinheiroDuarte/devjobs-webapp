291dbd7be5dcb3260d1c5efa8eff6099115be33f
Lionel Pinheiro Duarte <contact.pinheiro.lionel@gmail.com>
2023-04-12 16:28:45 +0200

    Import font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
 import '@/styles/globals.css'
+import { Kumbh_Sans } from 'next/font/google'
+
+const kumbh_sans = Kumbh_Sans({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
-  <>
+  <main className={kumbh_sans.className}>
     <h1>Change me to the global header component</h1>
     <Component {...pageProps} />
-  </>)
+  </main>)
 }
