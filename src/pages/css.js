d19053afea8db0d8e806d220a5c424d48e5265bf
Lionel Pinheiro Duarte <contact.pinheiro.lionel@gmail.com>
2023-04-13 14:47:53 +0200

    Create a css testing page and create css rules
    	- Add rules about colors and fonts
    	- Style the default headings
    	- Style list

---
+++ src/pages/css.js
@@ -0,0 +1,23 @@
+export default function test(){
+    return(
+        <>
+            <h1>Jived fox nymph grabs quick waltz</h1>
+            <h2>Jived fox nymph grabs quick waltz</h2>
+            <h3>Jived fox nymph grabs quick waltz</h3>
+            <h4>Jived fox nymph grabs quick waltz</h4>
+            <p class="">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. 
+            </p>
+            <ul className="list-style">
+                <li className="list-item-style secondary-color-dark-gray">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                <li className="list-item-style secondary-color-dark-gray">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                <li className="list-item-style secondary-color-dark-gray">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            </ul>
+            <ol className="list-style">
+                <li className="list-item-style secondary-color-dark-gray">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                <li className="list-item-style secondary-color-dark-gray">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                <li className="list-item-style secondary-color-dark-gray">Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus.</li>
+            </ol>
+        </>
+    )
+}
